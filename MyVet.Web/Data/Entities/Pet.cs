af3e656b6a80152238b7fe0dccfbe581fadfc61e
Eddyrvm <67993192+Eddyrvm@users.noreply.github.com>
2020-07-23 08:54:06 -0500

    CREACION DE ENTIDADES

--- MyVet.Web/Data/Entities/Pet.cs
+++ MyVet.Web/Data/Entities/Pet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,35 @@ namespace MyVet.Web.Data.Entities
 {
     public class Pet
     {
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+        public string Race { get; set; }
+
+        [Display(Name ="Born")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString ="{0:yyyy/MM/dd}", ApplyFormatInEditMode =true)]
+        public DateTime Born { get; set; }
+
+        public string Remarks { get; set; }
+
+        //  public ICollectionn<History> Hisories { get; set; }
+
+        public string ImageFullPath => string.IsNullOrEmpty(ImageUrl)
+            ? null
+            : $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+
+        [Display(Name = "Data")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime BornLocal => Born.ToLocalTime();
     }
 }
